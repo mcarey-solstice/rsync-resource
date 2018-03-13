22f02818697ac1c2c3edbc5a1d5b2ab97372c67c
Mike Carey <mcarey@solstice.com>
2018-03-13 13:02:38 -0500

    Refined test suite

--- tests/.init.sh
+++ tests/.init.sh
@@ -7,6 +7,13 @@ __DIR__="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 __OUT__="$__DIR__"/_output
 
 ###
+# Checks if a function exists
+##
+function_exists() {
+  [ `type -t $1`"" == 'function' ]
+}
+
+###
 # Provides the parent directory of the script that is calling this function
 #
 # Example:
@@ -14,7 +21,7 @@ __OUT__="$__DIR__"/_output
 #
 #   Output: check
 ##
-__script__() {
+__suite__() {
   echo $(basename "$( cd "$( dirname "${BASH_SOURCE[-1]}" )" && pwd )")
 }
 
@@ -43,6 +50,62 @@ __dir__() {
 }
 
 ###
+# Provides the path to the script that is to be run for that testing suite
+##
+__script__() {
+  echo "$__DIR__"/../scripts/"$(__suite__)"
+}
+
+###
+# Provides the file that can be logged to
+#
+# Parameters:
+#   1 {string=log} The extension to attach to the file
+##
+__outfile__() {
+  echo "$__OUT__"/"$(__suite__)"/"$(__caller__)".${1:-log}
+}
+
+###
+# Runs setup for a run
+##
+_pre_run() {
+  echo "Running $(__suite__)/$(__caller__)"
+
+  mkdir -p "$__OUT__"/"$(__suite__)"
+}
+
+###
+# Runs cleanup for a run
+##
+_post_run() {
+  echo "$(__suite__)/$(__caller__) passed"
+}
+
+###
+# Runs the main if defined.  If no main defined, the first parameter can be the name of a function that is defined and will be run with all parameters passed in.  If neither, the Default function is run
+#
+# Environment:
+#   main {function} If defined, it will be the function to run
+#
+# Parameters:
+#   1 {string} The function to run and all args passed to the function
+##
+run() {
+  _pre_run
+
+  if function_exists main; then
+    eval "main $@"
+  elif function_exists ${1:-}; then
+    eval "$@"
+  else
+    Default
+  fi
+
+  _post_run
+}
+
+###
 # Runs the test for the script that called this function.  It sends the output of that script to an output file and compares it with a json file in the same directory as the script.
 #
 # Example:
@@ -51,16 +114,50 @@ __dir__() {
 #   Calls the `../scripts/check` script and redirects the output to `tests/_output/check/test_1.json`
 #   Then runs a diff against `tests/check/test_1.json`
 ##
-run() {
-  echo "Running $(__script__)/$(__caller__)"
+Default() {
+  "$(__script__)" > "$(__outfile__ json)"
+  EXT=json ShouldMatch
+}
 
-  mkdir -p "$__OUT__"/"$(__script__)"
+###
+# Checks that two files are the same
+#
+# Environment:
+#   EXT {string=log} The extension for default a and b
+#
+# Parameters:
+#     The files to compare
+#   1 {string=$__OUT__/$(__script__)/$(__caller__).$ext}
+#   2 {string=$(__dir__)/$(__caller__).$ext}
+##
+ShouldMatch() {
+  local ext="${EXT:-log}"
+  local a="${1:-$(__outfile__ $ext)}"
+  local b="${2:-$(__dir__)/$(__caller__).$ext}"
 
-  "$__DIR__"/../scripts/"$(__script__)" > "$__OUT__"/"$(__script__)"/"$(__caller__)".json
+  echo "Comparing $a and $b"
+  diff -q "$a" "$b"
 
-  diff -q "$__OUT__"/"$(__script__)"/"$(__caller__)".json "$(__dir__)"/"$(__caller__)".json
+  echo "$a and $b match"
+}
 
-  echo "$(__script__)/$(__caller__) passed"
+###
+# Checks that the script throws an error
+##
+ShouldThrow() {
+  local outfile="$(__outfile__)"
+
+  set +e
+
+  "$(__script__)" > "$outfile"
+  if [ $? -eq 0 ]; then
+    echo "Expected $(__suite__) to exit with a nonzero status" 1>&2
+    exit 255
+  fi
+
+  echo "$(__suite__) exited with a nonzero status"
+
+  set -e
 }
 
 # tests
